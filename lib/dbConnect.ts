6bdecb7f9817302322c88896f3d465b409c49bf6
Amit <amittambulkar104@gmail.com>
2024-10-30 17:05:55 +0530

    chore: make db connection

---
+++ lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+
+    console.log(db);
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log(db.connections);
+
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.error("Database connection failed", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
